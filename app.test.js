1981830f451793e8ee135d8f41c93d64c7e2213f
Kristijonas <krnorkus@gmail.com>
2026-04-21 16:22:15 +0300

    added mocked response from db, misc pipe change

--- app.test.js
+++ app.test.js
@@ -1,7 +1,24 @@
 const request = require("supertest");
 const app = require("./app");
 
+const Person = require("./models/person");
+jest.mock("./models/person");
+
 describe("testing /api/persons", () => {
+  beforeEach(() => {
+    Person.find.mockResolvedValue([
+      {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: "1",
+      },
+      {
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: "2",
+      },
+    ]);
+  });
   it("returns status code 200", async () => {
     const res = await request(app).get("/api/persons");
 
@@ -18,6 +35,7 @@ describe("testing /api/persons", () => {
   it("returns valid persons list/array", async () => {
     const res = await request(app).get("/api/persons");
 
+    // console.log(res.body);
     await res.body.forEach((person) => {
       expect(person).toHaveProperty("id");
       expect(person).toHaveProperty("name");
